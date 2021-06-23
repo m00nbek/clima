fcf087dffbdf2c6a0b706ce375e8286e45a6d75f
m00nbek <melikulov990@gmail.com>
2021-06-23 11:17:05 +0500

    Integrated Alamofire

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 protocol WeatherManagerDelegate {
     func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel)
     func didFailWithError(error: Error)
@@ -25,29 +26,20 @@ struct WeatherManager {
     }
     var delegate: WeatherManagerDelegate?
     func performRequest(with urlString: String) {
-        if let url = URL(string: urlString) {
-            let session = URLSession(configuration: .default)
-            let task = session.dataTask(with: url) { (data, response, error) in
-                if error != nil {
-                    delegate?.didFailWithError(error: error!)
-                    return
-                }
-                if let safeData = data {
-                    if let weather = parseJSON (safeData) {
-                        delegate?.didUpdateWeather(self, weather: weather)
-                    }
-                    
-                }
+        AF.request(urlString).response { response in
+            if response.error != nil {
+                self.delegate?.didFailWithError(error: response.error!)
+                return
             }
-            task.resume()
-            
+            guard let data = response.data else {return}
+            if let weather = parseJSON(data) {
+                self.delegate?.didUpdateWeather(self, weather: weather)
+            }
         }
-        
     }
     func parseJSON(_ weatherData: Data) -> WeatherModel? {
-        let decoder = JSONDecoder()
         do {
-            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            let decodedData = try JSONDecoder().decode(WeatherData.self, from: weatherData)
     
             let id = decodedData.weather[0].id
             let temp = decodedData.main.temp
@@ -61,5 +53,4 @@ struct WeatherManager {
             return nil
         }
     }
-    
 }
